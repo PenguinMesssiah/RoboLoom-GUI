3d037030f81eda70694106ae3cb68d70165e3e62
SHREDLabComputer <shredlabcomputer@gmail.com>
2022-05-16 11:59:47 -0400

    Updated Serial Connection
    Serial connection now tries com 5 and com 6 to see if there's a board and exits if it finds nothing.

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ numMotors = 16
 numFrames = 4
 pattern_length2 = 20
 
-arduino = serial.Serial(port='COM6', baudrate=115200, timeout=.1)
+try:
+    arduino = serial.Serial(port='COM6', baudrate=115200, timeout=.1)
+except serial.serialutil.SerialException:
+    print("Com 6 not available, trying Com 5")
+    try:
+        arduino = serial.Serial(port='COM5', baudrate=115200, timeout=.1)
+    except serial.serialutil.SerialException:
+        print("Com 5 not available, exiting")
+        quit()
+
+
 
 arduino.flush()
 init_serial(arduino, numMotors)
