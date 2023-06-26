2f769c37cd1889db9028dbb661f1d044634ff8d4
Will Scott <wns5@pitt.edu>
2023-06-26 11:59:50 -0400

    Commented Out Serial Check

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from serialCom import init_serial
 from constants import *
 import serial.tools.list_ports
 
+connected = False
+arduino = None
+"""
 ports = serial.tools.list_ports.comports()
 print(ports)
 port_names = []
 for port, desc, hwid in sorted(ports):
     port_names.append(port)
 print(port_names)
-connected = False
-arduino = None
 for port in port_names:
     try:
         arduino = serial.Serial(port=port, baudrate=115200, timeout=.1)
         connected = True
     except serial.serialutil.SerialException:
         print(port + " not available, continuing without connection")
-
+"""
 numMotors = NUM_MOTORS
 numFrames = 4
 numPedals = 6
